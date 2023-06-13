cc6468c2f88e79bfccb5e1071aa56d417a1fdeda
Edward West <edward_job@163.com>
2023-06-13 11:43:34 +0800

    feat: start from 0 coding react code

---
+++ react-record/02_learn_react_scaffold/src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class HelloWorld extends React.Component {
+  render() {
+    return (
+    <div>
+      <h2>Hello World</h2>
+      <p>Hello World! Hello React!</p>
+    </div>
+    );
+  }
+}
+
+export default HelloWorld;
