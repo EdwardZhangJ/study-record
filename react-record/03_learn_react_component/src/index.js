6e26421c03c3a5cbf58ce8374c83fff5698d704d
Edward West <edward_job@163.com>
2023-06-14 15:26:10 +0800

    feat: add react class component + function component

--- react-record/03_learn_react_component/src/index.js
+++ react-record/03_learn_react_component/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App_class';
-import App from './01_class-function_component/App_func';
+import App from './01_component-class&function/App_func';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
