51179c4d7de38636eceb0c84bc2dfa2d8f538639
Edward West <edward_job@163.com>
2023-06-15 18:59:41 +0800

    feat: add react-slot-usage

--- react-record/03_learn_react_component/src/index.js
+++ react-record/03_learn_react_component/src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from 'react-dom/client';
 // import App from './02_component-life-cycle/App'
 // import App from './03_component-nesting-relationship/App'
 // import App from './04_component-communication-progress/App'
-import App from './05_demo-component-communication/App'
+// import App from './05_demo-component-communication/App'
+import App from './06_slot-usage/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
