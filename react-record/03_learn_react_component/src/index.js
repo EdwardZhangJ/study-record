eb1d3f20fe2dcff5b1f3a8ae5917f120549c389f
Edward West <edward_job@163.com>
2023-06-15 11:11:51 +0800

    feat: add components-nesting-relationship

--- react-record/03_learn_react_component/src/index.js
+++ react-record/03_learn_react_component/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App_class';
 // import App from './01_component-class&function/App_func';
-import App from './02_component-life-cycle/App'
+// import App from './02_component-life-cycle/App'
+// import App from './03_component-nesting-relationship/App'
+import App from './04_component-communication-progress/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <App /> );
