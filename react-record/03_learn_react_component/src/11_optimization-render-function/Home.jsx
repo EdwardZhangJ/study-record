34e9aeda463fcf79a893295c4917c654f746b828
Edward West <edward_job@163.com>
2023-06-21 14:38:38 +0800

    feat: react PureComponent

--- react-record/03_learn_react_component/src/11_optimization-render-function/Home.jsx
+++ react-record/03_learn_react_component/src/11_optimization-render-function/Home.jsx
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-export class Home extends Component {
+export class Home extends PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      friend: []
+    }
+  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // 对比state是否发生改变：this.state.friend !== nextState.friend
+  //   if(this.state.friend !== nextState.friend) { return true}
+  //   if(this.props.message !== nextProps.message) { return true}
+  //   return false
+  // }
   render() {
     console.log('Home render');
     return (
       <div>
-        <h2>Home Page</h2>
+        <h2>Home Page - {this.props.message}</h2>
       </div>
     )
   }
