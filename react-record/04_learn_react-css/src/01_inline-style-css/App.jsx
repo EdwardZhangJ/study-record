65a374c822e2d25a1f6f46af47389d442b9e8585
Edward West <edward_job@163.com>
2023-07-10 11:37:00 +0800

    feat: add react CSS use files

---
+++ react-record/04_learn_react-css/src/01_inline-style-css/App.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react'
+
+export class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2 style={{color: 'red', fontSize: '30px'}}>我是标题</h2>
+        <p style={{color: 'blue', fontSize: '20px'}}>我是内容 哈哈哈哈</p>
+      </div>
+    )
+  }
+}
+
+export default App
