ef76e70ecccfef4386468f439a2b16f434c8c06e
Edward West <edward_job@163.com>
2023-07-14 15:20:04 +0800

    feat: add redux use in react project

---
+++ react-record/06_learn_redux-in-react/src/store/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  counter: 0
+}
+
+function reducer(state=initialState, action) {
+  switch (action.type) {
+    case 'value':
+      
+      break;
+  
+    default:
+      break;
+  }
+}
