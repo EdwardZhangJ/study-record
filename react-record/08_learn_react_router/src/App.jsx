6ded02a343aa3cac31eb6d568406a3028b77c208
Edward West <edward_job@163.com>
2023-07-26 14:33:59 +0800

    feat: init react-router use Navigate

--- react-record/08_learn_react_router/src/App.jsx
+++ react-record/08_learn_react_router/src/App.jsx
@@ -1,9 +1,15 @@
 import React, { PureComponent } from 'react'
-import {Link, NavLink, Route, Routes} from 'react-router-dom'
+import { Link, Navigate, NavLink, Route, Routes} from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import './index.css'
+
+// Link, NavLink
+
 export class App extends PureComponent {
+   
+
   render() {
     return (
       <div className="App">
@@ -18,14 +24,18 @@ export class App extends PureComponent {
 
           <NavLink to='/home' style={({isActive})=>({color: isActive ? 'red' : ''})}>首页</NavLink>
           <NavLink to='/about' style={({isActive})=>({color: isActive ? 'red' : ''})}>关于</NavLink>
+
+          <Link to='/login'>登陆</Link>
         </div>
         <hr />
       </div>
       <div className="content">
         {/* 映射关系： path => Component  */}
         <Routes>
+          <Route path='/' element={<Navigate to='/home' />}/>
           <Route path='/home' element={<Home />}/>
           <Route path='/about' element={<About />}/>
+          <Route path='/login' element={<Login />}/>
         </Routes>
       </div>
 
