8b66edeba1a94eea96e1d2c772b0deef973a7161
Edward West <edward_job@163.com>
2023-07-27 14:59:47 +0800

    feat: init react-hooks

--- react-record/09_learn_react_hooks/src/App.js
+++ react-record/09_learn_react_hooks/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HelloWord extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+  render() {
+    return <div>Hello World</div>
+  }
 }
 
-export default App;
+export class App extends PureComponent {
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
