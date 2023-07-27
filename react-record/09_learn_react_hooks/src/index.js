a993caba2ce617acfdc268829b856c986d3577fc
Edward West <edward_job@163.com>
2023-07-27 17:13:01 +0800

    feat: init react-hooks demo-counter-comparison

--- react-record/09_learn_react_hooks/src/index.js
+++ react-record/09_learn_react_hooks/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+// import App from './01_no-use-hook/App'
+import App from './02_demo-counter-comparison/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
