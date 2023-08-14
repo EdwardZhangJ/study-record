c4962c1c7e2e9b5e33b0b5456e47366a2b3c8b02
Edward West <edward_job@163.com>
2023-08-14 18:33:04 +0800

    feat: add react-hooks custom Hooks demo - localStorage

--- react-record/09_learn_react_hooks/src/12_custom-Hooks/App.jsx
+++ react-record/09_learn_react_hooks/src/12_custom-Hooks/App.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo } from 'react'
 import { useLocalStorage } from './hooks'
 import './style.css'
 
--- react-record/09_learn_react_hooks/src/12_custom-Hooks/hooks/useLocalStorage.js
+++ react-record/09_learn_react_hooks/src/12_custom-Hooks/hooks/useLocalStorage.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react"
 
 function useLocalStorage(key) {
-  const [data, setData ] = useState(localStorage.getItem(key))
+  const [data, setData] = useState(() => {
+    const storageData = localStorage.getItem(key)
+    if (!storageData) return ("")
+    return JSON.parse(storageData)
+  })
 
   useEffect(() => {
-    localStorage.setItem(key, data)
-  }, [data])
+    localStorage.setItem(key, JSON.stringify(data))
+  }, [key, data])
 
   return [data, setData]
 }
