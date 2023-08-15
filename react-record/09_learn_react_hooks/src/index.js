228f10fc8f709888b5fe3901143e8c66abedc727
Edward West <edward_job@163.com>
2023-08-15 11:59:09 +0800

    feat: add react-redux-hooks construct the origin use of redux and the redux-hooks

--- react-record/09_learn_react_hooks/src/index.js
+++ react-record/09_learn_react_hooks/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { UserContext, ThemeContext } from './05_useContext-use/context'
 import { UserContext, TokenContext } from './12_custom-Hooks/context'
+import { Provider } from 'react-redux'
+import store from './13_redux-hooks/store'
 
 // import App from './App';
 // import App from './01_no-use-hook/App'
@@ -15,14 +17,18 @@ import { UserContext, TokenContext } from './12_custom-Hooks/context'
 // import App from './09_useRef-use/App'
 // import App from './10_useImperativeHandle-use/App'
 // import App from './11_useLayoutEffect-use/App'
-import App from './12_custom-Hooks/App'
+// import App from './12_custom-Hooks/App'
+import App from './13_redux-hooks/App'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <UserContext.Provider value={{name: 'why', level: 99}}>
     <TokenContext.Provider value={ 'coderwhy' }>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </TokenContext.Provider>
   </UserContext.Provider>
 
