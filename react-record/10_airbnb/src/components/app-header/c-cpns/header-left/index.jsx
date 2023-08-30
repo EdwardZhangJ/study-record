4e7ce204dc6fd175917c0995adbed566c5b639e5
Edward West <edward_job@163.com>
2023-08-30 17:19:14 +0800

    feat: Project Airbnb set header-left logo.svg

--- react-record/10_airbnb/src/components/app-header/c-cpns/header-left/index.jsx
+++ react-record/10_airbnb/src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,11 +1,12 @@
 import React, { memo } from 'react'
 import { LeftWrapper } from './style'
+import IconLogo from '@/assets/svg/icon_logo'
 
 const HeaderLeft = memo(() => {
   return (
     <div>
       <LeftWrapper>
-        HeaderLeft
+        <IconLogo />
       </LeftWrapper>
     </div>
 
