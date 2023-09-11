7f2a08d52ede9db93814a02edce3e869a136f8a2
Edward West <edward_job@163.com>
2023-09-11 19:59:09 +0800

    feat: add home content fetch Data

--- react-record/10_airbnb/src/views/home/index.jsx
+++ react-record/10_airbnb/src/views/home/index.jsx
@@ -1,12 +1,15 @@
 import React, { memo, useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 
-import edRequest from '@/services'
 import { HomeWrapper } from './style'
 import HomeBanner from './c-cpns/home-banner'
 import { fetchHomeDataAction } from '@/store/modules/home'
 
 const Home = memo(() => {
+  // 从redux中获取数据
+   const {goodPriceInfo} = useSelector((state) => ({
+    goodPriceInfo: state.home.goodPriceInfo
+  }), shallowEqual)
 
   // 定义状态
   const dispatch = useDispatch()
