0d28bb30914ab8386c1c82a10cb0939f0a8a3162
edward <edward_job@163.com>
2023-10-17 15:24:10 +0800

    :sparkles: feat: add Nodejs developed Node server frame Express Basic use

--- nodejs-record/06_Node-service-express/01_express-basic-use.js
+++ nodejs-record/06_Node-service-express/01_express-basic-use.js
@@ -4,7 +4,17 @@ const express = require('express')
 // 1. 创建express服务器
 const app = express()
 
+// 客户端访问URL： /login 和 /home
+app.post('/login', (req, res) => {
+  // 处理login请求
+  res.end('login success')
+})
+
+app.get('/home', (req, res) => {
+  res.end('home page data')
+})
+
 // 2. 启动服务器，并且监听端口
-app.listen(8000, () => {
-  console.log('服务器启动成功，请访问：http://localhost:8000')
+app.listen(9000, () => {
+  console.log('服务器启动成功，请访问：http://localhost:9000')
 })
