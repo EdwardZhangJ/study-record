da8ddbb1bb906dbb0eb0f9cb53d851e98ca3cde2
edward <edward_job@163.com>
2023-10-19 15:22:14 +0800

    :sparkles: feat: add Nodejs developed Node server frame Express multer library -> file upload

---
+++ nodejs-record/06_Node-service-express/12_application-upload-file.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const multer = require('multer')
+
+// 创建app对象
+const app = express()
+
+// 应用express提供的第三方中间件 multer
+const upload = multer({ 
+  // dest: './uploads' 
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads')
+    },
+    filename: (req, file, cb) => { // 保持文件的原始名称
+      cb(null, file.originalname)
+    }
+  })
+})
+
+// 编写中间件
+// 上传单文件：single方法
+app.post('/avatar', upload.single('avatar'), (req, res, next) => {
+  console.log('upload file info', req.file);
+  res.end('upload file success')
+})
+
+// 启动服务器
+app.listen(9000, () => {
+  console.log('服务器启动成功~')
+})
