71199c74080e964d48ec927279acbd488b6b2590
edward <edward_job@163.com>
2023-12-21 16:45:57 +0800

    :sparkles: feat: add Nodejs developed MySQL - mysql2 connection pool

--- nodejs-record/09_MySQL-database-driver/03_mysql2-connect-pool.js
+++ nodejs-record/09_MySQL-database-driver/03_mysql2-connect-pool.js
@@ -1,20 +1,20 @@
 
 const mysql = require('mysql2')
 
-// 1. 创建一个链接（连接上数据库）
-const connection = mysql.createConnection({
+// 1. 创建一个链接
+const connectionPool = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   database: 'music_db',
   user: 'root',
   password: 'Edward1879220',
+  connectionLimit: 5,
 })
 
 // 2. 执行一个SQL语句：预处理语句
 const statement = 'SELECT * FROM students WHERE age > ?'
-connection.execute(statement, [18], (err, results, fields) => {
+connectionPool.execute(statement, [18], (err, results, fields) => {
   if (err) throw err
   console.log(results)
 })
 
-connection.destroy()
