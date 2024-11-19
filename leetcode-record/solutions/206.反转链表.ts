d1b0334b7412f57a60ef01b812086c95d22654c0
edward <edward_job@163.com>
2024-11-19 23:56:54 +0800

    feat: :sparkles: 添加  leetcode  反转链表

---
+++ leetcode-record/solutions/206.反转链表.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=typescript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+    // 1. 迭代
+    let prev = null
+    let curr = head
+
+    while(curr) {
+      let next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+    }
+
+    return prev
+};
+// @lc code=end
+
