3e8b969ef1729c922a0ee7064b8e2a0273e8dbea
edward <edward_job@163.com>
2025-08-13 11:21:58 +0800

    feat: :sparkles: 添加 ty minusone

---
+++ typescript-record/type-challenges/02257-medium-minusone.ts
@@ -0,0 +1,33 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<MinusOne<1>, 0>>,
+  Expect<Equal<MinusOne<55>, 54>>,
+  Expect<Equal<MinusOne<3>, 2>>,
+  Expect<Equal<MinusOne<100>, 99>>,
+  Expect<Equal<MinusOne<1101>, 1100>>,
+  Expect<Equal<MinusOne<9_007_199_254_740_992>, 9_007_199_254_740_991>>,
+]
+
+
+// ============= Your Code Here =============
+// 仅支持小于等于 1100 的数字
+// type MinusOneSmall<N extends number, T extends any[] = []> =
+//   [...T, any]['length'] extends N ? T['length'] : MinusOneSmall<N, [...T, any]>
+
+// type MinusOne<N extends number> = MinusOneSmall<N>
+type ParseInt<T extends string> = T extends `${infer Digit extends number}` ? Digit : never
+
+type ReverseString<S extends string> = S extends `${infer First}${infer Rest}` ? `${ReverseString<Rest>}${First}` : ''
+
+type RemoveLeadingZeros<S extends string> = S extends '0' ? S : S extends `${'0'}${infer R}` ? RemoveLeadingZeros<R> : S
+
+type InternalMinusOne<
+  S extends string
+> = S extends `${infer Digit extends number}${infer Rest}` ?
+  Digit extends 0 ?
+  `9${InternalMinusOne<Rest>}` :
+  `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}` :
+  never
+type MinusOne<T extends number> = ParseInt<RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>>
